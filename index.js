968fc4867102b019376876d50fde0f37108e8248
ligia sell <ligia.sell@hotmail.com>
2019-11-25 19:13:25 -0300

    Add BE Vacininha

---
+++ index.js
@@ -0,0 +1,23 @@
+// Import express
+const express = require('express')
+var bodyParser = require('body-parser');
+const apiRoutes = require("./api-routes")
+const morgan = require('morgan')
+
+// Initialize the app
+let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+     res.header("Access-Control-Allow-Origin", "*");
+     res.header("Access-Control-Allow-Headers", "*"); 
+     next();
+});
+app.use(morgan('short'))
+app.use('/', apiRoutes)
+// Setup server port
+var port = process.env.PORT || 8081;
+// Send message for default URL
+app.listen(port, function () {
+     console.log("Running Vacininha API on port " + port);
+});
--- src/utils/mocks/userList.js
+++
@@ -1,9 +0,0 @@
-export default [{
-  id: 1,
-  name: 'Miguel',
-  email: 'miguel@vacininha.com',
-  password: 'cGFja2lvdDIwMTg=',//packiot2018
-  profile: {
-    id: 1
-  }
-}];
